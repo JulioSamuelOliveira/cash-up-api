7c4733b8907cdeefbcd498c127d3a162555d907f
JulioSamuelOliveira <juliosamuoliveira@gmail.com>
2025-04-10 09:49:17 -0300

    Adição de paginação e filtros de pesquisa.

--- src/main/java/br/com/fiap/cash_up_api/controller/TransactionController.java
+++ src/main/java/br/com/fiap/cash_up_api/controller/TransactionController.java
@@ -1,48 +1,36 @@
 package br.com.fiap.cash_up_api.controller;
- 
- import java.math.BigDecimal;
- import java.time.LocalDate;
- import java.util.List;
- 
- import org.springframework.beans.factory.annotation.Autowired;
- import org.springframework.data.domain.Example;
- import org.springframework.data.domain.ExampleMatcher;
- import org.springframework.data.domain.ExampleMatcher.StringMatcher;
- import org.springframework.web.bind.annotation.GetMapping;
- import org.springframework.web.bind.annotation.RequestMapping;
- import org.springframework.web.bind.annotation.RestController;
- 
- import br.com.fiap.cash_up_api.model.Transaction;
- import br.com.fiap.cash_up_api.repository.TransactionRepository;
- import lombok.extern.slf4j.Slf4j;
- 
- @RestController
- @RequestMapping("transactions")
- @Slf4j
- public class TransactionController {
- 
-     record TransactionFilter(String description, LocalDate date, BigDecimal amount){}
- 
-     @Autowired
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.cash_up_api.model.Transaction;
+import br.com.fiap.cash_up_api.repository.TransactionRepository;
+import br.com.fiap.cash_up_api.specification.TransactionSpecification;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("transactions")
+@Slf4j
+public class TransactionController {
+
+    public record TransactionFilter(String description, LocalDate startDate, LocalDate enDate) {}
+
+    @Autowired
      private TransactionRepository repository;
- 
-     @GetMapping
-     public List<Transaction> index(TransactionFilter filter){
-         log.info("Buscando transações com descrição {} e data {}", filter.description(), filter.date());
- 
-         var probe = Transaction.builder()
-                         .description(filter.description())
-                         .date(filter.date())
-                         .amount(filter.amount())
-                         .build();
- 
-         var matcher = ExampleMatcher
-                             .matchingAll()
-                             .withIgnoreCase()
-                             .withStringMatcher(StringMatcher.CONTAINING);
-         
-         var example = Example.of(probe, matcher);
-         return repository.findAll(example);
+
+    @GetMapping
+     public Page<Transaction> index(TransactionFilter filter, @PageableDefault(size = 10, sort = "date", direction = Direction.DESC) Pageable pageable){ //para mostrar todos os dados separados em páginas, em ordem pela data
+
+        var specification = TransactionSpecification.withFilters(filter);
+         return repository.findAll(specification, pageable);
      }
      
  }
